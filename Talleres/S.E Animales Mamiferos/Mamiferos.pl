81b938a7a815f089bfc5c19b204cb01ebca209ca
Álvaro Alejandro Zarabanda Gutiérrez <aazarabandag@correo.udistrital.edu.co>
2022-05-15 22:52:55 -0500

    prueba con predicados estaticos

--- Talleres/S.E Animales Mamiferos/Mamiferos.pl
+++ Talleres/S.E Animales Mamiferos/Mamiferos.pl
@@ -1,1 +1,20 @@
-prueba(a,b).
+%Deduccion de algunos animales mamiferos
+
+:- write("Digite 'ejecutar.' ").
+
+ejecutar :- verifica(Animal), write("El animal es: "),write(Animal).
+
+
+
+
+%hechos y reglas
+onza:- false.
+tigre:- true.
+
+%se verifica el animal
+verifica(onza):-onza,!.
+verifica(tigre):-tigre,!.
+verifica(cebra):-cebra,!.
+verifica(jirafa):-jirafa,!.
+verifica(desconocido).
+
