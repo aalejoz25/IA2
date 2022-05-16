507ec574573f1e2b60eea00a123775b05f6295f3
Álvaro Alejandro Zarabanda Gutiérrez <aazarabandag@correo.udistrital.edu.co>
2022-05-16 00:13:32 -0500

    reglas de la onza añadidas

--- Talleres/S.E Animales Mamiferos/Mamiferos.pl
+++ Talleres/S.E Animales Mamiferos/Mamiferos.pl
@@ -4,12 +4,7 @@
 
 ejecutar :- verifica(Animal), write("El animal es: "),write(Animal).
 
-
-
-
-%hechos y reglas
-onza:- false.
-tigre:- true.
+/*hechos y reglas*/
 
 %se verifica el animal
 verifica(onza):-onza,!.
@@ -17,4 +12,32 @@ verifica(tigre):-tigre,!.
 verifica(cebra):-cebra,!.
 verifica(jirafa):-jirafa,!.
 verifica(desconocido).
+
+/*Verificacion de las caracteristicas del animal*/
+
+% Primero se verifica si el usuario no ha respondido anteriormente a la
+% pregunta
+onza:- comprobar(tiene_color_leonado),
+       comprobar(tiene_manchas_oscuras).
+tigre:- true.
+
+%Se pregunta al usuario en caso de que no se haya
+:- dynamic si/1, no/1.
+
+preguntar(P):-
+    write("El animal "), write(P), write("? "),nl,
+    read(Respuesta),
+    ((Respuesta==si)->(assert(si(P)));fail->(assert(no(P)))).
+
+%Este bloque comprueba si la pregunta ya se agrego a la base de reglas
+comprobar(Pregunta):-
+    (si(Pregunta)->true).
+comprobar(Pregunta):-
+    (no(Pregunta)->false).
+comprobar(Pregunta):- preguntar(Pregunta).
+
+
+
+
+
 
